74c2ace20933e0ae4cbf623be8c23e18590022dd
Varun Sharda <varunsh@amdocs.com>
2018-03-27 16:21:32 +0530

    removing unnecessary file

--- src/main/java/com/employee/config/SecurityWebInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.employee.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
